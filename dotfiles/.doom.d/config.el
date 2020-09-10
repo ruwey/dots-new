9d220bd6f80a58dc2375af85d1337de5454c0edd
ruwey <gd@ruwey.com>
2020-09-10 16:05:30 -0400

    Switched to Iosevka Ruwey in a number of places

--- dotfiles/.doom.d/config.el
+++ dotfiles/.doom.d/config.el
@@ -19,7 +19,11 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
+{%@@ if profile == "bigbrother" @@%}
+(setq doom-font (font-spec :family "Iosevka Ruwey" :size 14))
+{%@@ else @@%}
+(setq doom-font (font-spec :family "Anonymous Pro" :size 14))
+{%@@ endif @@%}
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
