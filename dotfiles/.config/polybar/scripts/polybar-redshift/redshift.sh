3ee0e64d9927f80782e883e33b9080aa2e058761
ruwey <gd@ruwey.com>
2021-03-22 18:15:26 -0400

    Added new polybar config

---
+++ dotfiles/.config/polybar/scripts/polybar-redshift/redshift.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# Adopted from: https://github.com/VineshReddy/polybar-redshift
+
+envFile=~/.config/polybar/scripts/polybar-redshift/env.sh
+changeValue=300
+
+source $envFile
+
+changeMode() {
+  sed -i "s/REDSHIFT=$1/REDSHIFT=$2/g" $envFile 
+  REDSHIFT=$2
+  echo $REDSHIFT
+}
+
+changeTemp() {
+  if [ "$2" -gt 1000 ] && [ "$2" -lt 25000 ]
+  then
+    sed -i "s/REDSHIFT_TEMP=$1/REDSHIFT_TEMP=$2/g" $envFile 
+    redshift -P -O $2
+  fi
+}
+
+case $1 in 
+  toggle) 
+    if [ "$REDSHIFT" = on ];
+    then
+      changeMode "$REDSHIFT" off
+      redshift -x
+    else
+      changeMode "$REDSHIFT" on
+      redshift -P -O "$REDSHIFT_TEMP"
+    fi
+    ;;
+  increase)
+    changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP+changeValue))
+    ;;
+  decrease)
+    changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP-changeValue));
+    ;;
+  temperature)
+    case $REDSHIFT in
+      on)
+        printf "%dK" "$REDSHIFT_TEMP"
+        ;;
+      off)
+        printf "off"
+        ;;
+    esac
+    ;;
+  set)
+    changeMode "$REDSHIFT" on
+    changeTemp $((REDSHIFT_TEMP)) $2
+    ;;
+esac
